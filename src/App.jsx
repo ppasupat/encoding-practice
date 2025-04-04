6f225159e393d5ead6fb34750e0ec7649659b5a1
Panupong Pasupat <panuponggong@gmail.com>
2025-04-03 18:41:32 -0700

    Reduce the weights of the index-based questions

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import Quiz from './Quiz';
 export default function App() {
   const [score, setScore] = useState({right: 0, wrong: 0});
   const [questionPatternWeights, setQuestionPatternWeights] = useState({
+    'alphabet -> index': 0.2,
+    'index -> alphabet': 0.2,
+    'alphabet -> binary': 0.4,
+    'binary -> alphabet': 0.4,
+    'alphabet -> ternary': 0.4,
+    'ternary -> alphabet': 0.4,
   });
 
   return <Quiz
